3ea7f956f37da4dc470de360708b66a776902274
Thomas Roth <code@stacksmashing.net>
2018-03-21 17:22:05 +0100

    Fix build for 64 bit and update .gitignore

--- build_analyzer.py
+++ build_analyzer.py
@@ -41,7 +41,7 @@ os.chdir( ".." )
 #specify the search paths/dependencies/options for gcc
 include_paths = [ "./AnalyzerSDK/include" ]
 link_paths = [ "./AnalyzerSDK/lib" ]
-link_dependencies = [ "-lAnalyzer" ] #refers to libAnalyzer.dylib or libAnalyzer.so
+link_dependencies = [ "-lAnalyzer64" ] #refers to libAnalyzer.dylib or libAnalyzer.so
 
 debug_compile_flags = "-O0 -w -c -fpic -g"
 release_compile_flags = "-O3 -w -c -fpic"
